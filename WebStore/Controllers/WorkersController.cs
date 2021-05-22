5a0dc4dcfceddb98339cc29800d76b5eecbea8d9
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-05-22 11:09:51 +0400

    Вывод сотрудников работает

---
+++ WebStore/Controllers/WorkersController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private static readonly IEnumerable<Worker> __Workers = Worker.GetWorkers;
+
+        public IActionResult Index()
+        {
+            return View(__Workers);
+        }
+    }
+}
---
+++ WebStore/Models/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Models.Base;
+
+namespace WebStore.Models
+{
+    public class Worker : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public int CountClildren { get; set; }
+
+        public static List<Worker> GetWorkers => Enumerable.Range(1, 10).Select(p => new Worker
+        {
+            Id = p,
+            FirstName = $"Иван_{p}",
+            LastName = $"Иванов_{p + 1}",
+            Patronymic = $"Иванович_{p + 2}",
+            Age = p + 20,
+            Birthday = new DateTime(1980 + p, 1, 1),
+            EmploymentDate = DateTime.Now.AddYears(- p).AddMonths(p),
+            CountClildren = p,
+        }).ToList();
+    }
+}
