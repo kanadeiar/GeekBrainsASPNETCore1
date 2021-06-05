86b6aa67b8e16f0910bf9098e66f13122ecb5f3a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-05 19:57:22 +0400

    Маршрут приветствия

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,9 @@ namespace WebStore
             app.UseRouting();
 
             app.UseMiddleware<DebugMiddleware>();
+
+            app.Map("/HelloGeekbrains",
+                context => context.Run(async request => await request.Response.WriteAsync("Hello Geekbrains!")));
 
             app.UseEndpoints(endpoints =>
             {
