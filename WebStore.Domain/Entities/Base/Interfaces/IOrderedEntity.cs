5c9042e1660bdcc43f7735667c71f03b076c1af1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-08 19:10:01 +0400

    Чистка хлама

--- WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
+++ WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.Domain.Entities.Base.Interfaces
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
 {
     public interface IOrderedEntity : IEntity
     {
--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebStore.Domain.Entities.Base;
+﻿using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
