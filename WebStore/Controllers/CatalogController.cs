cd59e838466c457d1f0096066ee7bd4ca5346a62
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-09 01:43:27 +0400

    Контроллер каталога товаров +

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _productData;
+        public CatalogController(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IActionResult Index(int? brandId, int? sectionId)
+        {
+            return View();
+        }
+    }
+}
--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -11,11 +11,13 @@ namespace WebStore.Services
     {
         private readonly List<Section> _Sections;
         private readonly List<Brand> _Brands;
+        private readonly List<Product> _Products;
 
         public InMemoryProductData(TestData testData)
         {
             _Sections = testData.GetSections.ToList();
             _Brands = testData.GetBrands.ToList();
+            _Products = testData.GetProducts.ToList();
         }
 
         public IEnumerable<Section> GetSections() => _Sections;
@@ -24,7 +26,12 @@ namespace WebStore.Services
 
         public IEnumerable<Product> GetProducts(ProductFilter productFilter = null)
         {
-            return null;
+            IEnumerable<Product> query = _Products;
+            if (productFilter?.SectionId is { } sectionId)
+                query = query.Where(p => p.SectionId == sectionId);
+            if (productFilter?.BrandId is { } brandId)
+                query = query.Where(p => p.BrandId == brandId);
+            return query;
         }
     }
 }
