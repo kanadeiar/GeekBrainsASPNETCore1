bddf6bb99b4627ecb348184c08b4fa2d790ec7e2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-09 01:15:01 +0400

    Визуальный компонент категорий товаров

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+        public SectionsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var all = _productData.GetSections();
+
+            var parents = all.Where(p => p.ParentId == null);
+            var patentsViews = parents.Select(p => new SectionViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Order = p.Order
+            }).ToList();
+
+            foreach (var patentsView in patentsViews)
+            {
+                var children = all.Where(c => c.ParentId == patentsView.Id);
+                foreach (var child in children)
+                {
+                    patentsView.Children.Add(new SectionViewModel
+                    {
+                        Id = child.Id,
+                        Name = child.Name,
+                        Order = child.Order,
+                        Parent = patentsView,
+                    });
+                }
+                patentsView.Children.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+            patentsViews.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+
+            return View(patentsViews);
+        }
+    }
+}
---
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class SectionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public SectionViewModel Parent { get; set; }
+        public List<SectionViewModel> Children { get; set; } = new();
+    }
+}
