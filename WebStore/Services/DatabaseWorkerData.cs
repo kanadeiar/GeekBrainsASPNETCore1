377517cc81b1242fc8dabdd8b5c089b2d3cd1ced
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-13 00:13:39 +0400

    Начало создания места жительства работников в базе данных

---
+++ WebStore/Services/DatabaseWorkerData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class DatabaseWorkerData : IWorkerData
+    {
+        public IEnumerable<Worker> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Worker Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Worker worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Worker worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
