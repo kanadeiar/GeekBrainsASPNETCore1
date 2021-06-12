a6e4b7050117cb23700b9dbab4d43abcf182405c
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-12 20:01:27 +0400

    Создание контроллера с заглушками

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
