25b284d4a5c5f18bdc97c4165ffd521fece1736f
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-14 00:16:22 +0400

    Баннер входа/выхода пользователей работает

---
+++ WebStore/Components/UserInfoViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (User.Identity?.IsAuthenticated == true)
+                return View("UserInfo");
+            return View();
+        }
+    }
+}
