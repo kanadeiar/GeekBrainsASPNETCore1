7631dca221680ab0793cefa6880bcc3132c97141
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-15 07:27:23 +0400

    Базовая часть сервиса корзины в куках

---
+++ WebStore/Services/InCookiesCartService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+using WebStore.WebModels;
+
+namespace WebStore.Services
+{
+    public class InCookiesCartService : ICartService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IProductData _productData;
+        private readonly string _cartName;
+        private readonly Mapper _mapperProductToView = new (new MapperConfiguration(c => c.CreateMap<Product, ProductWebModel>()
+            .ForMember("Section", o => o.MapFrom(p => p.Section.Name))
+            .ForMember("Brand", o => o.MapFrom(p => p.Brand.Name))));
+
+        private Cart Cart
+        {
+            get
+            {
+                var context = _httpContextAccessor.HttpContext;
+                var cookies = context!.Response.Cookies;
+                var cartCookie = context.Request.Cookies[_cartName];
+                if (cartCookie is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(_cartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+                ReplaceCookies(cookies, cartCookie);
+                return JsonConvert.DeserializeObject<Cart>(cartCookie);
+            }
+            set => ReplaceCookies(_httpContextAccessor.HttpContext!.Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+        private void ReplaceCookies(IResponseCookies cookies, string cookie)
+        {
+            cookies.Delete(_cartName);
+            cookies.Append(_cartName, cookie);
+        }
+
+        public InCookiesCartService(IHttpContextAccessor contextAccessor, IProductData productData)
+        {
+            _httpContextAccessor = contextAccessor;
+            _productData = productData;
+
+            var user = _httpContextAccessor.HttpContext!.User;
+            var userName = user.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
+
+            _cartName = $"KanadeiarWebStore.Cart{userName}";
+        }
+        public void Add(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MinusOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartWebModel GetWebModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebStore/Services/Interfaces/ICartService.cs
+++ WebStore/Services/Interfaces/ICartService.cs
@@ -5,7 +5,7 @@ namespace WebStore.Services.Interfaces
     public interface ICartService
     {
         void Add(int id);
-        void Minus(int id);
+        void MinusOne(int id);
         void Remove(int id);
         void Clear();
         CartWebModel GetWebModel();
