6be1735137669e550e22f361380cfe7f399ad9d2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-16 02:05:24 +0400

    Контроллер корзины сделан и подключен

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public IActionResult Index()
+        {
+            return View(_cartService.GetWebModel());
+        }
+        public IActionResult Add(int id)
+        {
+            _cartService.Add(id);
+            return RedirectToAction("Index", "Cart");
+        }
+        public IActionResult Subtract(int id)
+        {
+            _cartService.Subtract(id);
+            return RedirectToAction("Index", "Cart");
+        }
+        public IActionResult Remove(int id)
+        {
+            _cartService.Remove(id);
+            return RedirectToAction("Index", "Cart");
+        }
+        public IActionResult Clear()
+        {
+            _cartService.Clear();
+            return RedirectToAction("Index", "Cart");
+        }
+    }
+}
--- WebStore/Services/InCookiesCartService.cs
+++ WebStore/Services/InCookiesCartService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -70,7 +69,7 @@ namespace WebStore.Services
             Cart = cart;
         }
 
-        public void MinusOne(int id)
+        public void Subtract(int id)
         {
             var cart = Cart;
 
