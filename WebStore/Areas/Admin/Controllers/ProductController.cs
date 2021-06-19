5e83909f30709d113da1bff51139fc001753675a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-19 04:58:24 +0400

    ++ Редактирование товаров в админке

---
+++ WebStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = Role.Administrators)]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
