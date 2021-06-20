a33aa09d9a7c1d4fd7fa9b9884737e35f6081fa2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-20 07:52:22 +0400

    Профиль пользователя показывается

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        public UserProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
