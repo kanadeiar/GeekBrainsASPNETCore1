901d5f038557f0896ba3440515524a304de5e0c9
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-25 01:50:19 +0400

    Контроллер тестовых данных WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> __testList = Enumerable
+            .Range(1, 20)
+            .Select(i => $"Тестовое значение № {i}")
+            .ToList();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(__testList);
+
+        [HttpGet("count")]
+        public IActionResult GetCount() => Ok(__testList.Count);
+
+        //[HttpGet("id[[{id:int}]]")]
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id > __testList.Count)
+                return NotFound();
+            return Ok(__testList[id]);
+        }
+
+        //[HttpPost("add")]
+        [HttpPost]
+        public IActionResult Add(string str)
+        {
+            __testList.Add(str);
+            return Ok();
+        }
+
+        //[HttpPut("edit/{id:int}")]
+        [HttpPut("{id:int}")]
+        public IActionResult Replace(int id, string str)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id > __testList.Count)
+                return NotFound();
+            __testList[id] = str;
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id > __testList.Count)
+                return NotFound();
+            __testList.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
