291e57be5e79858a41d8950baf1f57dec6061313
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-25 19:00:14 +0400

    Клиент к тестовому сервису готов

---
+++ Services/WebStore.Interfaces/WebAPI/IValuesService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.Interfaces.WebAPI
+{
+    public interface IValuesService
+    {
+        IEnumerable<string> GetAll();
+        string GetById(int id);
+        void Add(string str);
+        void Edit(int id, string str);
+        bool Delete(int id);
+    }
+}
--- Services/WebStore.WebAPI.Client/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Client/Base/BaseClient.cs
@@ -1,6 +1,15 @@
-﻿namespace WebStore.WebAPI.Client.Base
+﻿using System.Net.Http;
+
+namespace WebStore.WebAPI.Client.Base
 {
     public abstract class BaseClient
     {
+        protected readonly HttpClient _client;
+        protected readonly string _address;
+        public BaseClient(HttpClient client, string address)
+        {
+            _client = client;
+            _address = address;
+        }
     }
 }
