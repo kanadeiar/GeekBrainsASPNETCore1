8a7e0ea13f7fa1175848420950e6b626c0abd1f7
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-01 20:23:30 +0400

    Начало апи контроллера и клиента

---
+++ Services/WebStore.WebAPI.Client/Person/PersonApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using WebStore.WebAPI.Client.Base;
+
+namespace WebStore.WebAPI.Client.Person
+{
+    public class PersonApiClient : BaseSyncClient
+    {
+        public PersonApiClient(HttpClient client, string address) : base(client, address) { }
+
+
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/PersonApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Adresses;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [Route(WebAPIInfo.Persons), ApiController]
+    public class PersonApiController : ControllerBase
+    {
+        private readonly IWorkerData _workerData;
+        public PersonApiController(IWorkerData workerData)
+        {
+            _workerData = workerData;
+        }
+
+
+
+
+    }
+}
