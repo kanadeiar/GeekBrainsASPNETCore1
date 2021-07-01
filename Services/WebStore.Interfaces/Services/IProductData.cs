5ec6f7f0db804e44c292eadfddf1f181d8e95b51
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-01 21:45:29 +0400

    Небольшая доработка

--- Services/WebStore.Interfaces/Services/IProductData.cs
+++ Services/WebStore.Interfaces/Services/IProductData.cs
@@ -8,14 +8,20 @@ namespace WebStore.Interfaces.Services
     {
         /// <summary> Все категории товаров </summary>
         IEnumerable<Section> GetSections();
-        /// <summary> Все бренды </summary>
-        IEnumerable<Brand> GetBrands();
-        /// <summary> Все товары с фильтрацией по категориям и/или брендам </summary>
-        IEnumerable<Product> GetProducts(IProductFilter productFilter = null, bool includes = false);
+        /// <summary> Одна категория </summary>
+        /// <param name="id">Идентификатор</param>
+        Section GetSection(int id);
         /// <summary> Категории товаров с включениями продуктов </summary>
         IEnumerable<Section> GetSectionsWithProducts();
+        /// <summary> Все бренды </summary>
+        IEnumerable<Brand> GetBrands();
+        /// <summary> Один бренд </summary>
+        /// <param name="id">Идентификатор</param>
+        Brand GetBrand(int id);
         /// <summary> Бренды с включениями продуктов </summary>
         IEnumerable<Brand> GetBrandsWithProducts();
+        /// <summary> Все товары с фильтрацией по категориям и/или брендам </summary>
+        IEnumerable<Product> GetProducts(IProductFilter productFilter = null, bool includes = false);
         /// <summary> Получение одного товара по ид </summary>
         Product GetProductById(int id);
         /// <summary> Добавить товар </summary>
