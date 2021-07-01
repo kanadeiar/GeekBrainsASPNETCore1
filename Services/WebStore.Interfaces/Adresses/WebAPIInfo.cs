2950656180f11ea118156c407cfe6c74fb81e41b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-01 19:57:12 +0400

    Вынос адресов апи контроллера в отдельный класс

---
+++ Services/WebStore.Interfaces/Adresses/WebAPIInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.Interfaces.Adresses
+{
+    /// <summary> Ссылки на адреса апи контроллера </summary>
+    public static class WebAPIInfo
+    {
+        /// <summary> Адрес апи сотрудников </summary>
+        public const string Persons = "api/persons";
+    }
+}
