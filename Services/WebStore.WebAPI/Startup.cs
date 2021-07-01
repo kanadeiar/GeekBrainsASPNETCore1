13f104d6a1c7dac78406bdcb909766eb9726b9bf
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-01 19:48:37 +0400

    Копирование сервисов из веб приложения в веб апи

--- Services/WebStore.WebAPI/Startup.cs
+++ Services/WebStore.WebAPI/Startup.cs
@@ -1,9 +1,19 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WebStore.Dal.Context;
+using WebStore.Dal.DataInit;
+using WebStore.Dal.Interfaces;
+using WebStore.Domain.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Data;
+using WebStore.Services.Services;
 
 namespace WebStore.WebAPI
 {
@@ -17,6 +27,65 @@ namespace WebStore.WebAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var databaseName = Configuration["Database"];
+            switch (databaseName)
+            {
+                case "MSSQL": 
+                    services.AddDbContext<WebStoreContext>(opt => 
+                        opt.UseSqlServer(Configuration.GetConnectionString("MSSQL"),
+                            o => o.MigrationsAssembly("WebStore.Dal")));
+                    break;
+                case "SQLite":
+                    services.AddDbContext<WebStoreContext>(opt =>
+                        opt.UseSqlite(Configuration.GetConnectionString("SQLite"),
+                            o => o.MigrationsAssembly("WebStore.Dal.Sqlite")));
+                    break;
+            }
+
+            services.AddTransient<IWebStoreDataInit, WebStoreDataInit>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(o =>
+            {
+#if DEBUG
+                o.Password.RequireDigit = false;
+                o.Password.RequiredLength = 3;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredUniqueChars = 3;
+#endif
+                o.User.RequireUniqueEmail = false;
+                o.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                o.Lockout.AllowedForNewUsers = false;
+                o.Lockout.MaxFailedAccessAttempts = 10;
+                o.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            });
+
+            services.ConfigureApplicationCookie(o =>
+            {
+                o.Cookie.Name = "KanadeiarWebStore";
+                o.Cookie.HttpOnly = true;
+                o.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+                o.LoginPath = "/Account/Login";
+                o.LogoutPath = "/Account/Logout";
+                o.AccessDeniedPath = "/Account/AccessDenied";
+
+                o.SlidingExpiration = true;
+            });
+
+            services.AddSingleton<TestData>();
+
+            services.AddScoped<ICartService, InCookiesCartService>();
+            services.AddScoped<IProductData, DatabaseProductData>();
+            services.AddScoped<IWorkerData, DatabaseWorkerData>();
+            services.AddScoped<IOrderService, DatabaseOrderService>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -26,6 +95,8 @@ namespace WebStore.WebAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
