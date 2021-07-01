763274d838686a2f534fc25b59488e7c87834c02
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-01 20:39:09 +0400

    API контроллер и клиент готов

--- Services/WebStore.WebAPI.Client/Person/PersonApiClient.cs
+++ Services/WebStore.WebAPI.Client/Person/PersonApiClient.cs
@@ -1,12 +1,41 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
 using WebStore.WebAPI.Client.Base;
 
 namespace WebStore.WebAPI.Client.Person
 {
-    public class PersonApiClient : BaseSyncClient
+    public class PersonApiClient : BaseSyncClient, IWorkerData
     {
         public PersonApiClient(HttpClient client, string address) : base(client, address) { }
 
+        public IEnumerable<Worker> GetAll()
+        {
+            return Get<IEnumerable<Worker>>(_address);
+        }
 
+        public Worker Get(int id)
+        {
+            return Get<Worker>($"{_address}/{id}");
+        }
+
+        public int Add(Worker worker)
+        {
+            var response = Post(_address, worker);
+            return response.Content.ReadFromJsonAsync<int>().Result;
+        }
+
+        public void Update(Worker worker)
+        {
+            Put(_address, worker);
+        }
+
+        public bool Delete(int id)
+        {
+            var result = Delete($"{_address}/{id}").IsSuccessStatusCode;
+            return result;
+        }
     }
 }
--- Services/WebStore.WebAPI/Controllers/PersonApiController.cs
+++ Services/WebStore.WebAPI/Controllers/PersonApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
 using WebStore.Interfaces.Adresses;
 using WebStore.Interfaces.Services;
 
@@ -13,8 +14,37 @@ namespace WebStore.WebAPI.Controllers
             _workerData = workerData;
         }
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_workerData.GetAll());
+        }
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_workerData.Get(id));
+        }
 
+        [HttpPost]
+        public IActionResult Add(Worker worker)
+        {
+            var id = _workerData.Add(worker);
+            return Ok(id);
+        }
 
+        [HttpPut]
+        public IActionResult Update(Worker worker)
+        {
+            _workerData.Update(worker);
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _workerData.Delete(id);
+            return result ? Ok(true) : NotFound(false);
+        }
     }
 }
