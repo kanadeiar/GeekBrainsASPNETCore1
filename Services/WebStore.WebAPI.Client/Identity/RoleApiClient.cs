a4d5e42cb6dc0a7e4d75e0ca1305b48c626b6852
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-05 07:29:05 +0400

    Базовые определения для веб апи клиентов

---
+++ Services/WebStore.WebAPI.Client/Identity/RoleApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using WebStore.Interfaces.Adresses;
+using WebStore.WebAPI.Client.Base;
+
+namespace WebStore.WebAPI.Client.Identity
+{
+    public class RoleApiClient : BaseClient
+    {
+        public RoleApiClient(HttpClient client) : base(client, WebAPIInfo.Identity.ApiRole) { }
+
+
+
+    }
+}
---
+++ Services/WebStore.WebAPI.Client/Identity/UserApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using WebStore.Interfaces.Adresses;
+using WebStore.WebAPI.Client.Base;
+
+namespace WebStore.WebAPI.Client.Identity
+{
+    public class UserApiClient : BaseClient
+    {
+        public UserApiClient(HttpClient client) : base(client, WebAPIInfo.Identity.ApiUser) { }
+
+
+
+    }
+}
