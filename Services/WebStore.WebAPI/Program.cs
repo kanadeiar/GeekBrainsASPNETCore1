42ebf02b041acdce69d431c1e91b2bbef0657bec
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-15 20:20:08 +0400

    Работает логирование в веб апи

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,7 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore.WebAPI
 {
@@ -14,16 +18,24 @@ namespace WebStore.WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging((host, log) => log
-                        .ClearProviders()
-                        .AddDebug()
-                        .AddEventLog()
-                        .AddConsole()
-                        .AddFilter<ConsoleLoggerProvider>(level => level >= LogLevel.Information)
-                    //.AddFilter<ConsoleLoggerProvider>((category, level) => level >= LogLevel.Information && category.EndsWith("Info"))
-                )
+                //.ConfigureLogging((host, log) => log
+                //        .ClearProviders()
+                //        .AddDebug()
+                //        .AddEventLog()
+                //        .AddConsole()
+                //        .AddFilter<ConsoleLoggerProvider>(level => level >= LogLevel.Information)
+                //    //.AddFilter<ConsoleLoggerProvider>((category, level) => level >= LogLevel.Information && category.EndsWith("Info"))
+                //)
                 .ConfigureWebHostDefaults(webBuilder => webBuilder
-                        .UseStartup<Startup>()
+                        .UseStartup<Startup>())
+                .UseSerilog((host, log) => log
+                    .ReadFrom.Configuration(host.Configuration)
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(outputTemplate:"[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                    .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                    .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
                 )
             ;
     }
