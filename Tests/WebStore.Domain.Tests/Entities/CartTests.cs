db93d4d965fd617726e873f2b6b2176aca825077
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-18 17:20:36 +0400

    Добавлено тестирование моделей для корзины

---
+++ Tests/WebStore.Domain.Tests/Entities/CartTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.Tests.Entities
+{
+    [TestClass]
+    public class CartTests
+    {
+        private Cart _cart;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cart = new Cart
+            {
+                Items = Enumerable.Range(1, 5).Select(i => new CartItem()
+                {
+                    ProductId = i,
+                    Quantity = i,
+                }).ToList()
+            };
+        }
+
+        [TestMethod]
+        public void ItemsCount_Returns_Correct()
+        {
+            var cart = _cart;
+            var expectedCount = _cart.Items.Sum(i => i.Quantity);
+
+            var actualCount = cart.ItemsSum;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
---
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+using WebStore.Domain.WebModels;
+using WebStore.Domain.WebModels.Cart;
+
+namespace WebStore.Services.Tests.Services
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _cart;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cart = new Cart
+            {
+                Items = Enumerable.Range(1, 5).Select(i => new CartItem()
+                {
+                    ProductId = i,
+                    Quantity = i,
+                }).ToList()
+            };
+        }
+
+
+
+
+
+
+    }
+}
