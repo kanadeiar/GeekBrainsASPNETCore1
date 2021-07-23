7c6424bc665b58ac419b75d8c461bf3332a30e6b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-24 03:13:59 +0400

    Модульные тесты работают

--- Tests/WebStore.Tests/Controllers/AccountControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/AccountControllerTests.cs
@@ -13,6 +13,7 @@ using Moq;
 using WebStore.Controllers;
 using WebStore.Domain.Identity;
 using WebStore.Domain.WebModels.Account;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace WebStore.Tests.Controllers
 {
@@ -97,15 +98,23 @@ namespace WebStore.Tests.Controllers
 
             var result = controller.Register(model).Result;
 
-            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            Assert
+                .IsInstanceOfType(result, typeof(RedirectToActionResult));
             var redirectResult = (RedirectToActionResult)result;
-            Assert.AreEqual("Home", redirectResult.ControllerName);
-            Assert.AreEqual(nameof(HomeController.Index), redirectResult.ActionName);
-            userManagerMock.Verify(u => u.CreateAsync(It.IsAny<User>(), It.IsAny<string>()));
-            userManagerMock.Verify(u => u.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()));
-            userManagerMock.Verify();
-            signInManagerMock.Verify(s => s.SignInAsync(It.IsAny<User>(), It.IsAny<bool>(), null));
-            signInManagerMock.Verify();
+            Assert
+                .AreEqual("Home", redirectResult.ControllerName);
+            Assert
+                .AreEqual(nameof(HomeController.Index), redirectResult.ActionName);
+            userManagerMock
+                .Verify(u => u.CreateAsync(It.IsAny<User>(), It.IsAny<string>()));
+            userManagerMock
+                .Verify(u => u.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()));
+            userManagerMock
+                .Verify();
+            signInManagerMock
+                .Verify(s => s.SignInAsync(It.IsAny<User>(), It.IsAny<bool>(), null));
+            signInManagerMock
+                .Verify();
         }
 
         [TestMethod]
@@ -114,7 +123,7 @@ namespace WebStore.Tests.Controllers
             var expectedTypeError = string.Empty;
             var expectedName = "TestName";
             var expectedPassword = "123";
-            var expectedErrorCode = "test";
+            var expectedErrorCode = "Произошла неизвестная ошибка";
             var expectedErrorDescription = "TestDescription";
             var loggerStub = Mock
                 .Of<ILogger<AccountController>>();
@@ -150,9 +159,11 @@ namespace WebStore.Tests.Controllers
                 .IsFalse(controller.ModelState.IsValid);
             var returnErrors = controller.ModelState[expectedTypeError].Errors;
             Assert
-                .AreEqual(expectedErrorCode, errors.FirstOrDefault().Code);
-            Assert
-                .AreEqual(expectedErrorDescription, errors.FirstOrDefault().Description);
+                .AreEqual(expectedErrorCode, returnErrors.FirstOrDefault().ErrorMessage);
+            userManagerMock
+                .Verify(u  => u.CreateAsync(It.IsAny<User>(), It.IsAny<string>()));
+            userManagerMock
+                .Verify();
         }
 
         #endregion
@@ -162,7 +173,7 @@ namespace WebStore.Tests.Controllers
         [TestMethod]
         public void Login_Returns_Correct()
         {
-            var expectedReturnUrl = "testUrl";
+            const string expectedReturnUrl = "testUrl";
             var loggerStub = Mock
                 .Of<ILogger<AccountController>>();
             var controller = new AccountController(new UserManagerMock(), new SignInManagerMock(), loggerStub);
@@ -178,13 +189,219 @@ namespace WebStore.Tests.Controllers
             Assert.AreEqual(expectedReturnUrl, loginModel.ReturnUrl);
         }
 
+        [TestMethod]
+        public void LoginModelInvalid_Returns_View()
+        {
+            const string expectedErrorCode = "Test";
+            const string expectedErrorMessage = "Message";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            LoginWebModel expectedModel = new LoginWebModel();
+            var controller = new AccountController(new UserManagerMock(), new SignInManagerMock(), loggerStub);
+            controller.ModelState.AddModelError(expectedErrorCode, expectedErrorMessage);
 
+            var result = controller.Login(expectedModel).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert
+                .IsInstanceOfType(viewResult.Model, typeof(LoginWebModel));
+        }
+
+        [TestMethod]
+        public void LoginModelReturnUrl_Returns_Redirect()
+        {
+            const string expectedUserName = "TestUser";
+            const string expectedPassword = "123";
+            const string expectedReturnUrl = "testUrl";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var signInManagerMock = new Mock<SignInManagerMock>();
+            signInManagerMock
+                .Setup(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                    It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Success);
+            LoginWebModel model = new LoginWebModel
+            {
+                UserName = expectedUserName,
+                Password = expectedPassword,
+                ReturnUrl = expectedReturnUrl,
+            };
+            var controller = new AccountController(new UserManagerMock(), signInManagerMock.Object, loggerStub);
+
+            var result = controller.Login(model).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(LocalRedirectResult));
+            var redirectResult = (LocalRedirectResult) result;
+            Assert
+                .AreEqual(expectedReturnUrl, redirectResult.Url);
+            signInManagerMock
+                .Verify(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                It.IsAny<bool>()));
+            signInManagerMock
+                .Verify();
+        }
+
+        [TestMethod]
+        public void LoginModel_Returns_Redirect()
+        {
+            const string expectedUserName = "TestUser";
+            const string expectedPassword = "123";
+            const string expectedReturnUrl = "/";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var signInManagerMock = new Mock<SignInManagerMock>();
+            signInManagerMock
+                .Setup(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                    It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Success);
+            LoginWebModel model = new LoginWebModel
+            {
+                UserName = expectedUserName,
+                Password = expectedPassword,
+            };
+            var controller = new AccountController(new UserManagerMock(), signInManagerMock.Object, loggerStub);
+
+            var result = controller.Login(model).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(LocalRedirectResult));
+            var redirectResult = (LocalRedirectResult) result;
+            Assert
+                .AreEqual(expectedReturnUrl, redirectResult.Url);
+            signInManagerMock
+                .Verify(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                It.IsAny<bool>()));
+            signInManagerMock
+                .Verify();
+        }
+
+        [TestMethod]
+        public void LoginModelFailed_Returns()
+        {
+            var expectedErrorCode = "Ошибка в имени пользователя, либо в пароле при входе в систему Identity";
+            const string expectedUserName = "TestUser";
+            const string expectedPassword = "123";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var signInManagerMock = new Mock<SignInManagerMock>();
+            signInManagerMock
+                .Setup(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                    It.IsAny<bool>()))
+                .ReturnsAsync(SignInResult.Failed);
+            LoginWebModel model = new LoginWebModel
+            {
+                UserName = expectedUserName,
+                Password = expectedPassword,
+            };
+            var controller = new AccountController(new UserManagerMock(), signInManagerMock.Object, loggerStub);
+
+            var result = controller.Login(model).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(ViewResult));
+            Assert
+                .IsFalse(controller.ModelState.IsValid);
+            var returnErrors = controller.ModelState[string.Empty].Errors;
+            Assert
+                .AreEqual(expectedErrorCode, returnErrors.FirstOrDefault().ErrorMessage);
+            signInManagerMock
+                .Verify(s => s.PasswordSignInAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                It.IsAny<bool>()));
+            signInManagerMock
+                .Verify();
+        }
+
+        #endregion
+
+        #region Выход из системы и доступ отказан
+
+        [TestMethod]
+        public void LogoutReturnUrl_Returns_Redirect()
+        {
+            const string expectedName = "TestUser";
+            const string expectedReturnUrl = "testUrl";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var signInManagerMock = new Mock<SignInManagerMock>();
+            signInManagerMock
+                .Setup(s => s.SignOutAsync());
+            var controller = new AccountController(new UserManagerMock(), signInManagerMock.Object, loggerStub)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        User = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, expectedName) }))
+                    }
+                }
+            };
+
+            var result = controller.Logout(expectedReturnUrl).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(LocalRedirectResult));
+            var redirectResult = (LocalRedirectResult) result;
+            Assert
+                .AreEqual(expectedReturnUrl, redirectResult.Url);
+            signInManagerMock
+                .Verify(s => s.SignOutAsync());
+            signInManagerMock
+                .Verify();
+        }
+
+        [TestMethod]
+        public void Logout_Returns_Redirect()
+        {
+            const string expectedName = "TestUser";
+            const string expectedReturnUrl = "/";
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var signInManagerMock = new Mock<SignInManagerMock>();
+            signInManagerMock
+                .Setup(s => s.SignOutAsync());
+            var controller = new AccountController(new UserManagerMock(), signInManagerMock.Object, loggerStub)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        User = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, expectedName) }))
+                    }
+                }
+            };
+
+            var result = controller.Logout(null).Result;
+
+            Assert
+                .IsInstanceOfType(result, typeof(LocalRedirectResult));
+            var redirectResult = (LocalRedirectResult) result;
+            Assert
+                .AreEqual(expectedReturnUrl, redirectResult.Url);
+            signInManagerMock
+                .Verify(s => s.SignOutAsync());
+            signInManagerMock
+                .Verify();
+        }
+
+        [TestMethod]
+        public void AccessDenied_Returns_Correct()
+        {
+            var loggerStub = Mock
+                .Of<ILogger<AccountController>>();
+            var controller = new AccountController(new UserManagerMock(), new SignInManagerMock(), loggerStub);
+
+            var result = controller.AccessDenied();
+
+            Assert
+                .IsInstanceOfType(result, typeof(ViewResult));
+        }
 
         #endregion
     }
-
     
-
     #region Моки подмены настоящих классов
 
     public class UserManagerMock : UserManager<User>
