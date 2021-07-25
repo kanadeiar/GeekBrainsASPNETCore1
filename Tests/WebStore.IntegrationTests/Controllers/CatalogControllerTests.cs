bccb57118de9c2e88360999dd089f7aca396d2df
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 03:48:10 +0400

    Должен работать интеграционный тест веб приложения + веб апи с подменой контекста базы данных

--- Tests/WebStore.IntegrationTests/Controllers/CatalogControllerTests.cs
+++ Tests/WebStore.IntegrationTests/Controllers/CatalogControllerTests.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Moq.Protected;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Orders;
 using WebStore.Domain.Models;
+using WebStore.IntegrationTests.WebStoreWebAPITestDrivers.Controllers;
 using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Client;
 
 namespace WebStore.IntegrationTests.Controllers
 {
@@ -59,6 +64,38 @@ namespace WebStore.IntegrationTests.Controllers
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             productDataMock.Verify(_ => _.GetProducts(It.IsAny<ProductFilter>(), It.IsAny<bool>()));
             productDataMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task Index_SendRequest_UseApiDriver_ShouldOk()
+        {
+            var productDataDriver = new WebStoreContextDriver();
+            WebApplicationFactory<Startup> webHost = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        var descriptor = services
+                            .SingleOrDefault(d => d.ServiceType == typeof(IProductData));
+                        services.Remove(descriptor);
+                        var mockMessageHandler = new Mock<HttpMessageHandler>();
+                        mockMessageHandler.Protected()
+                            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                            .ReturnsAsync(() =>
+                            {
+                                var filter = new ProductFilter();
+                                return productDataDriver.GetProducts(filter).Result;
+                            });
+                        var client = new ProductApiClient(new HttpClient(mockMessageHandler.Object) { BaseAddress = new Uri("http://localost/") });
+
+                        services.AddTransient<IProductData>(_ => client);
+                    });
+                });
+            var httpClient = webHost.CreateClient();
+
+            var response = await httpClient.GetAsync("Catalog");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
     }
---
+++ Tests/WebStore.IntegrationTests/WebStoreWebAPITestDrivers/Controllers/WebStoreContextDriver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using WebStore.Dal.Context;
+using WebStore.Dal.Interfaces;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Orders;
+using WebStore.Domain.Models;
+
+namespace WebStore.IntegrationTests.WebStoreWebAPITestDrivers.Controllers
+{
+    /// <summary> Драйвер для веб апи сервиса </summary>
+    public class WebStoreContextDriver
+    {
+        public async Task<HttpResponseMessage> GetProducts(ProductFilter filter = null, int count = 3)
+        {
+            var webHost = new WebApplicationFactory<WebAPI.Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        var descriptor = services
+                            .SingleOrDefault(_ => _.ServiceType == typeof(DbContextOptions<WebStoreContext>));
+                        services.Remove(descriptor);
+                        services.AddDbContext<WebStoreContext>(options =>
+                        {
+                            options.UseInMemoryDatabase(nameof(WebStoreContext));
+                        });
+                        var descriptorInitializer =
+                            services.SingleOrDefault(_ => _.ServiceType == typeof(IWebStoreDataInit));
+                        services.Remove(descriptorInitializer);
+                        services.AddTransient(_ => Mock.Of<IWebStoreDataInit>());
+                    });
+                });
+            var testContext = webHost
+                .Services.CreateScope().ServiceProvider.GetService<WebStoreContext>();
+            testContext!.Products.AddRange(
+                Enumerable.Range(1, 3).Select(i => new Product
+                {
+                    Name = $"Товар_{i}",
+                    Section = new Section (),
+                    Brand = new Brand (),
+                    OrderItems = Array.Empty<OrderItem>(),
+                }));
+            await testContext!.SaveChangesAsync();
+            var httpClient = webHost.CreateClient();
+
+            var response = await httpClient.PostAsJsonAsync("Api/Product", filter);
+
+            return response;
+        }
+    }
+}
