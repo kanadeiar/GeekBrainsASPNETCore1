57e2c1bdf991f4d93d2096fa59337fe18ee491ae
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 20:21:22 +0400

    Завершение тах-хелпера

--- UI/WebStore/TagHelpers/MyActiveRouteTagHelper.cs
+++ UI/WebStore/TagHelpers/MyActiveRouteTagHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -12,6 +11,8 @@ namespace WebStore.TagHelpers
     public class MyActiveRouteTagHelper : TagHelper
     {
         private const string attributeName = "my-active-route";
+
+        private const string ingoreAction = "ignore-action";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -27,22 +28,49 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (IsActive())
+            var ignoreAction = output.Attributes.ContainsName(ingoreAction);
+
+            if (IsActive(ignoreAction))
                 MakeActive(output);
 
             output.Attributes.RemoveAll(attributeName);
         }
 
-        private bool IsActive()
+        private bool IsActive(bool ignoreAction)
         {
+            var routeValues = ViewContext.RouteData.Values;
+ 
+            var currentController = routeValues["controller"]?.ToString();
+            var currentAction = routeValues["action"]?.ToString();
 
+            if (!string.IsNullOrEmpty(Controller) && !string.Equals(Controller, currentController, StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            if (ignoreAction)
+                return true;
+
+            if (!string.IsNullOrEmpty(Action) && !string.Equals(Action, currentAction, StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!routeValues.ContainsKey(key) || routeValues[key]?.ToString() != value)
+                    return false;
 
             return true;
         }
 
         private static void MakeActive(TagHelperOutput output)
         {
-
+            var classAttribute = output.Attributes.FirstOrDefault(a => a.Name == "class");
+ 
+            if (classAttribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (classAttribute.Value.ToString()?.Contains("active") ?? false)
+                    return;
+                output.Attributes.SetAttribute("class", classAttribute.Value + " active");
+            }
         }
     }
 }
