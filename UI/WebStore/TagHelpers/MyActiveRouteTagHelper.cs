2fe6df9e34b97eccde23abb9c37df2d477bb3ac1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-26 20:14:11 +0400

    Начало таг-хелпера навигации

---
+++ UI/WebStore/TagHelpers/MyActiveRouteTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = attributeName)]
+    public class MyActiveRouteTagHelper : TagHelper
+    {
+        private const string attributeName = "my-active-route";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(attributeName);
+        }
+    }
+}
