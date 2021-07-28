d8aec19e07622244cecea049483cea764acfface
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-28 07:12:36 +0400

    Сорение хлебными крошками

--- Common/WebStore.Domain/WebModels/BreadCrumbsWebModel.cs
+++ Common/WebStore.Domain/WebModels/BreadCrumbsWebModel.cs
@@ -8,6 +8,12 @@ namespace WebStore.Domain.WebModels
     /// <summary> Веб модель отображения хлебных крошек </summary>
     public class BreadCrumbsWebModel
     {
+        /// <summary> Название контроллера в коде </summary>
+        public string Controller { get; set; }
+        /// <summary> Название контроллера обыкновенное </summary>
+        public string ControllerText { get; set; }
+        /// <summary> Действие контроллера </summary>
+        public string ControllerAction { get; set; }
         /// <summary> Категория товара </summary>
         public Section Section { get; set; }
         /// <summary> Бренд </summary>
