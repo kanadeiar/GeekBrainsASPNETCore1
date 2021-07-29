a8abe54e100d8023708c2978d0ed314ba59f3e2a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-07-29 05:08:11 +0400

    Визуальный компонент корзины для ajax

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+        public CartViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.ProductsInCartCount = (await _cartService.GetWebModel()).ItemsSum;
+            return View();
+        }
+    }
+}
