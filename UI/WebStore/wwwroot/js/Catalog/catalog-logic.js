110c016a4c50b056e34471cbc993d84cb3d49653
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-01 19:29:58 +0400

    Начало создания логики ajax пагинации

---
+++ UI/WebStore/wwwroot/js/Catalog/catalog-logic.js
@@ -0,0 +1,28 @@
+﻿
+Catalog = {
+    _properties: {
+        getProductPartialView: ""
+    },
+    
+    init: properties => {
+        $.extend(Catalog._properties, properties);
+
+        $(".pagination li a").click(Catalog.clickOnPagination);
+    },
+
+    clickOnPagination: e => {
+        e.preventDefault();
+
+        let button = this;
+        Catalog.showToolTip(button, "123");
+
+        alert("1");
+    },
+    
+    showToolTip: function(button, message) {
+        button.tooltip({ title: message }).tooltip("show");
+        setTimeout(function() {
+            button.tooltip("destroy");
+        }, 1000);
+    },
+}
