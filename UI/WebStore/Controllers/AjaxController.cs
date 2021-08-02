289ecd57e2f35269503a0d51977b113a05f444aa
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-02 08:38:47 +0400

    Рендеринг на стороне клиента по полученным от сервера json данным

--- UI/WebStore/Controllers/AjaxController.cs
+++ UI/WebStore/Controllers/AjaxController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebStore.Domain.WebModels.Ajax;
 
@@ -11,16 +12,29 @@ namespace WebStore.Controllers
             return View();
         }
 
-        public async Task<IActionResult> GetHtml(int? id, string message, int Delay = 1000)
+        public async Task<IActionResult> GetHtml(int? id, string message, int delay = 1000)
         {
-            await Task.Delay(Delay);
+            await Task.Delay(delay);
 
-            return PartialView("Partial/_AjaxDataViewPartial", 
+            return PartialView("Partial/_AjaxDataViewPartial",
                 new AjaxDataWebModel
                 {
                     Id = id ?? 1,
                     Message = message,
+                    ServerTime = DateTime.Now,
                 });
+        }
+
+        public async Task<IActionResult> GetJson(int? id, string message, int delay = 1000)
+        {
+            await Task.Delay(delay);
+
+            return Json(new
+            {
+                id = id ?? 1,
+                message = $"Ответное: id = {id ?? 0} message = {message ?? "<null>"}",
+                ServerTime = DateTime.Now,
+            });
         }
     }
 }
