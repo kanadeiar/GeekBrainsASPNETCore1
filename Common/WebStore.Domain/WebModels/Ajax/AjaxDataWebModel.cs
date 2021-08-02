44a1fe5fbf1b399c1fa5bd9b3232572cc928fa3c
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-02 07:16:31 +0400

    Подготовка контролера и вьюх для неневязчивого ajax

---
+++ Common/WebStore.Domain/WebModels/Ajax/AjaxDataWebModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebStore.Domain.WebModels.Ajax
+{
+    public class AjaxDataWebModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime ServerTime { get; set; }
+    }
+}
