14cecb83839b80d44f63965d38e629053e0cfde4
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-08 09:21:02 +0400

    Сервис отправки писем электронной почты

---
+++ Services/WebStore.Interfaces/Services/ISendEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface ISendEmailService
+    {
+        Task SendEmailAsync(string nameTo, string mailTo, string subject, string body);
+    }
+}
