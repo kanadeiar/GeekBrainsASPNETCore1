1518de4dfcc3d7496da7eb9bed69f8f89d67feae
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-08-08 10:14:32 +0400

    Отправка писем пользователю работает

---
+++ Common/WebStore.Domain/WebModels/SendEmailMessageWebModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace WebStore.Domain.WebModels
+{
+    /// <summary> Веб модель отправки сообщения по электронной почте </summary>
+    public class SendEmailMessageWebModel
+    {
+        /// <summary> Имя отправителя </summary>
+        [Display(Name = "Имя отправителя")]
+        [Required(ErrorMessage = "Не указано имя отправителя")]
+        public string NameFrom { get; set; }
+        /// <summary> Адрес отправителя </summary>
+        [Display(Name = "Адрес отправителя")]
+        [Required(ErrorMessage = "Не указан адрес отправителя")]
+        public string MailFrom { get; set; }
+        /// <summary> Адрес почты отправителя </summary>
+        [Display(Name = "Адрес почтового сервера")]
+        [Required(ErrorMessage = "Не указан адрес сервера")]
+        public string Address { get; set; }
+        /// <summary> Порт почтового сервера </summary>
+        [Display(Name = "Порт почтового сервера")]
+        [Required(ErrorMessage = "Не указан порт сервера")]
+        public int Port { get; set; }
+        /// <summary> Логин пользователя </summary>
+        [Display(Name = "Логин пользователя почтового сервера")]
+        [Required(ErrorMessage = "Не указан логин пользователя для входа на почтовый сервер")]
+        public string Login { get; set; }
+        /// <summary> Пароль пользователя </summary>
+        [Display(Name = "Пароль пользователя почтового сервера")]
+        [Required(ErrorMessage = "Не указан пароль пользователя для входа на почтовый сервер")]
+        public string Password { get; set; }
+        /// <summary> Имя получателя </summary>
+        [Display(Name = "Имя получателя")]
+        [Required(ErrorMessage = "Не указано имя получателя сообщения")]
+        public string NameTo { get; set; }
+        /// <summary> Адрес получателя </summary>
+        [Display(Name = "Адрес получателя")]
+        [Required(ErrorMessage = "Не указан адрес получателя сообщения")]
+        public string MailTo { get; set; }
+        /// <summary> Заголовок сообщения </summary>
+        [Display(Name = "Заголовок сообщения")]
+        [Required(ErrorMessage = "Не указан заголовок сообщения")]
+        public string Subject { get; set; }
+        /// <summary> Тело сообщения </summary>
+        [Display(Name = "Тело сообщения")]
+        [Required(ErrorMessage = "Не указано тело сообщения")]
+        public string Body { get; set; }
+    }
+}
--- UI/WebStore/Areas/Admin/Controllers/HomeController.cs
+++ UI/WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,15 +1,60 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebStore.Domain.Identity;
+using WebStore.Domain.WebModels;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrators)]
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult SendEmail()
+        {
+            var model = new SendEmailMessageWebModel
+            {
+                Subject = "Email сообщение пользователю",
+                Body = "Тестовое Email сообщение пользователю",
+            };
+            ViewBag.Success = false;
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendEmail(SendEmailMessageWebModel model, [FromServices] ILogger<SendEmailService> logger)
+        {
+            if (model is null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Success = false;
+                return View(model);
+            }
+
+            ISendEmailService service = new SendEmailService(model.NameFrom, model.MailFrom, model.Address, model.Port,
+                new NetworkCredential(model.Login, model.Password), logger);
+            await service.SendEmailAsync(model.NameTo, model.MailTo, model.Subject, model.Body);
+
+            ViewBag.Success = true;
+            var newModel = new SendEmailMessageWebModel
+            {
+                Subject = "Email сообщение пользователю",
+                Body = "Тестовое Email сообщение пользователю",
+            };
+            return View(newModel);
         }
     }
 }
